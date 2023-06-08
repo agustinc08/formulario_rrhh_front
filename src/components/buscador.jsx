e06bce7c53aca6718a143f5da0503d76ae6883a9
Marcelo Agustin Compiani Flores <marcelo.compiani.flores@pjn.gov.ar>
2023-06-08 10:13:57 -0300

    Cambios en el buscador , agregado de Respuestas.

--- src/components/buscador.jsx
+++ src/components/buscador.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   Container,
   Typography,
@@ -10,7 +10,7 @@ import {
   InputLabel,
   Box,
   Divider,
-} from '@material-ui/core';
+} from "@material-ui/core";
 import {
   Table,
   TableHead,
@@ -18,7 +18,7 @@ import {
   TableRow,
   TableCell,
   TableSortLabel,
-} from '@material-ui/core';
+} from "@material-ui/core";
 import "../css/global.css";
 import useStyles from "../styles/buscadorStyle";
 
@@ -26,31 +26,34 @@ const Buscador = () => {
   const [preguntas, setPreguntas] = useState([]);
   const [dependencias, setDependencias] = useState([]);
   const [respuestas, setRespuestas] = useState([]);
-  const [selectedPregunta, setSelectedPregunta] = useState('');
-  const [selectedDependencia, setSelectedDependencia] = useState('');
-  const [sortConfig, setSortConfig] = useState({ field: null, direction: 'asc' });
+  const [selectedPregunta, setSelectedPregunta] = useState("");
+  const [selectedDependencia, setSelectedDependencia] = useState("");
+  const [sortConfig, setSortConfig] = useState({
+    field: null,
+    direction: "asc",
+  });
   const classes = useStyles();
 
   useEffect(() => {
     const obtenerPreguntas = async () => {
       try {
-        const response = await fetch('http://localhost:3000/preguntas');
+        const response = await fetch("http://localhost:3000/preguntas");
         const data = await response.json();
         setPreguntas(data);
-        console.log('Preguntas obtenidas:', data);
+        console.log("Preguntas obtenidas:", data);
       } catch (error) {
-        console.error('Error al obtener las preguntas:', error);
+        console.error("Error al obtener las preguntas:", error);
       }
     };
 
     const obtenerDependencias = async () => {
       try {
-        const response = await fetch('http://localhost:3000/dependencias');
+        const response = await fetch("http://localhost:3000/dependencias");
         const data = await response.json();
         setDependencias(data);
-        console.log('Dependencias obtenidas:', data);
+        console.log("Dependencias obtenidas:", data);
       } catch (error) {
-        console.error('Error al obtener las dependencias:', error);
+        console.error("Error al obtener las dependencias:", error);
       }
     };
 
@@ -60,10 +63,16 @@ const Buscador = () => {
 
   const handleBuscarRespuestas = async () => {
     try {
-      let url = 'http://localhost:3000/respuestas';
+      let url = "http://localhost:3000/respuestas";
 
-      const preguntaId = selectedPregunta !== undefined && selectedPregunta !== '' ? parseInt(selectedPregunta) : null;
-      const dependenciaId = selectedDependencia !== undefined && selectedDependencia !== '' ? parseInt(selectedDependencia) : null;
+      const preguntaId =
+        selectedPregunta !== undefined && selectedPregunta !== ""
+          ? parseInt(selectedPregunta)
+          : null;
+      const dependenciaId =
+        selectedDependencia !== undefined && selectedDependencia !== ""
+          ? parseInt(selectedDependencia)
+          : null;
 
       if (preguntaId && dependenciaId) {
         url += `/${preguntaId}/${dependenciaId}`;
@@ -76,26 +85,26 @@ const Buscador = () => {
       const response = await fetch(url);
       const data = await response.json();
       setRespuestas(data);
-      console.log('Respuestas obtenidas:', data);
+      console.log("Respuestas obtenidas:", data);
     } catch (error) {
-      console.error('Error al buscar respuestas:', error);
+      console.error("Error al buscar respuestas:", error);
     }
   };
 
   const handlePreguntaChange = (event) => {
     setSelectedPregunta(event.target.value);
-    console.log('Pregunta seleccionada:', event.target.value);
+    console.log("Pregunta seleccionada:", event.target.value);
   };
 
   const handleDependenciaChange = (event) => {
     setSelectedDependencia(event.target.value);
-    console.log('Dependencia seleccionada:', event.target.value);
+    console.log("Dependencia seleccionada:", event.target.value);
   };
 
   const handleSort = (field) => {
-    let direction = 'asc';
-    if (sortConfig.field === field && sortConfig.direction === 'asc') {
-      direction = 'desc';
+    let direction = "asc";
+    if (sortConfig.field === field && sortConfig.direction === "asc") {
+      direction = "desc";
     }
     setSortConfig({ field, direction });
   };
@@ -103,22 +112,27 @@ const Buscador = () => {
   const sortedRespuestas =
     Array.isArray(respuestas) && respuestas.length > 0
       ? respuestas.sort((a, b) => {
-        const aValue = a[sortConfig.field] || '';
-        const bValue = b[sortConfig.field] || '';
+          const aValue = a[sortConfig.field] || "";
+          const bValue = b[sortConfig.field] || "";
 
-        if (sortConfig.direction === 'asc') {
-          return aValue.toString().localeCompare(bValue.toString());
-        } else {
-          return bValue.toString().localeCompare(aValue.toString());
-        }
-      })
+          if (sortConfig.direction === "asc") {
+            return aValue.toString().localeCompare(bValue.toString());
+          } else {
+            return bValue.toString().localeCompare(aValue.toString());
+          }
+        })
       : [];
 
   return (
     <div>
       <Container>
         <Box sx={{ paddingTop: 20 }}>
-          <Typography variant="h3" align="center" gutterBottom sx={{ paddingTop: 40 }}>
+          <Typography
+            variant="h3"
+            align="center"
+            gutterBottom
+            sx={{ paddingTop: 40 }}
+          >
             Buscador de Respuestas
           </Typography>
         </Box>
@@ -135,11 +149,12 @@ const Buscador = () => {
                 fullWidth
               >
                 <MenuItem value="">Todas las preguntas</MenuItem>
-                {preguntas.length > 0 && preguntas.map((pregunta) => (
-                  <MenuItem key={pregunta.id} value={pregunta.id}>
-                    {pregunta.descripcion}
-                  </MenuItem>
-                ))}
+                {preguntas.length > 0 &&
+                  preguntas.map((pregunta) => (
+                    <MenuItem key={pregunta.id} value={pregunta.id}>
+                      {pregunta.descripcion}
+                    </MenuItem>
+                  ))}
               </Select>
             </FormControl>
           </Grid>
@@ -154,11 +169,12 @@ const Buscador = () => {
                 fullWidth
               >
                 <MenuItem value="">Todas las dependencias</MenuItem>
-                {dependencias.length > 0 && dependencias.map((dependencia) => (
-                  <MenuItem key={dependencia.id} value={dependencia.id}>
-                    {dependencia.nombreDependencia}
-                  </MenuItem>
-                ))}
+                {dependencias.length > 0 &&
+                  dependencias.map((dependencia) => (
+                    <MenuItem key={dependencia.id} value={dependencia.id}>
+                      {dependencia.nombreDependencia}
+                    </MenuItem>
+                  ))}
               </Select>
             </FormControl>
           </Grid>
@@ -168,7 +184,8 @@ const Buscador = () => {
               color="primary"
               onClick={handleBuscarRespuestas}
               fullWidth
-              size="small">
+              size="small"
+            >
               Buscar
             </Button>
           </Grid>
@@ -180,90 +197,86 @@ const Buscador = () => {
           <TableRow>
             <TableCell>
               <TableSortLabel
-                active={sortConfig.field === 'id'}
-                direction={sortConfig.field === 'id' ? sortConfig.direction : 'asc'}
-                onClick={() => handleSort('id')}
+                active={sortConfig.field === "id"}
+                direction={
+                  sortConfig.field === "id" ? sortConfig.direction : "asc"
+                }
+                onClick={() => handleSort("id")}
               >
                 ID
               </TableSortLabel>
             </TableCell>
             <TableCell>
               <TableSortLabel
-                active={sortConfig.field === 'createdAt'}
-                direction={sortConfig.field === 'createdAt' ? sortConfig.direction : 'asc'}
-                onClick={() => handleSort('createdAt')}
+                active={sortConfig.field === "createdAt"}
+                direction={
+                  sortConfig.field === "createdAt"
+                    ? sortConfig.direction
+                    : "asc"
+                }
+                onClick={() => handleSort("createdAt")}
               >
                 Fecha
               </TableSortLabel>
             </TableCell>
             <TableCell>
               <TableSortLabel
-                active={sortConfig.field === 'dependenciaId'}
-                direction={sortConfig.field === 'dependenciaId' ? sortConfig.direction : 'asc'}
-                onClick={() => handleSort('dependenciaId')}
+                active={sortConfig.field === "dependenciaId"}
+                direction={
+                  sortConfig.field === "dependenciaId"
+                    ? sortConfig.direction
+                    : "asc"
+                }
+                onClick={() => handleSort("dependenciaId")}
               >
                 Dependencia
               </TableSortLabel>
             </TableCell>
             <TableCell>
               <TableSortLabel
-                active={sortConfig.field === 'edad'}
-                direction={sortConfig.field === 'edad' ? sortConfig.direction : 'asc'}
-                onClick={() => handleSort('edad')}
+                active={sortConfig.field === "edad"}
+                direction={
+                  sortConfig.field === "edad" ? sortConfig.direction : "asc"
+                }
+                onClick={() => handleSort("edad")}
               >
                 Edad
               </TableSortLabel>
             </TableCell>
             <TableCell>
               <TableSortLabel
-                active={sortConfig.field === 'genero'}
-                direction={sortConfig.field === 'genero' ? sortConfig.direction : 'asc'}
-                onClick={() => handleSort('genero')}
+                active={sortConfig.field === "genero"}
+                direction={
+                  sortConfig.field === "genero" ? sortConfig.direction : "asc"
+                }
+                onClick={() => handleSort("genero")}
               >
                 Género
               </TableSortLabel>
             </TableCell>
             <TableCell>
               <TableSortLabel
-                active={sortConfig.field === 'expresion'}
-                direction={sortConfig.field === 'expresion' ? sortConfig.direction : 'asc'}
-                onClick={() => handleSort('expresion')}
+                active={
+                  sortConfig.field === "expresion" ||
+                  sortConfig.field === "clasificaciones" ||
+                  sortConfig.field === "grado" ||
+                  sortConfig.field === "calificaciones"
+                }
+                direction={sortConfig.direction}
+                onClick={() => handleSort("expresion")}
               >
-                Expresion
+                Respuestas
               </TableSortLabel>
             </TableCell>
             <TableCell>
               <TableSortLabel
-                active={sortConfig.field === 'calificaciones'}
-                direction={sortConfig.field === 'calificaciones' ? sortConfig.direction : 'asc'}
-                onClick={() => handleSort('calificaciones')}
-              >
-                Calificaciones
-              </TableSortLabel>
-            </TableCell>
-            <TableCell>
-              <TableSortLabel
-                active={sortConfig.field === 'clasificaciones'}
-                direction={sortConfig.field === 'clasificaciones' ? sortConfig.direction : 'asc'}
-                onClick={() => handleSort('clasificaciones')}
-              >
-                Clasificaciones
-              </TableSortLabel>
-            </TableCell>
-            <TableCell>
-              <TableSortLabel
-                active={sortConfig.field === 'grado'}
-                direction={sortConfig.field === 'grado' ? sortConfig.direction : 'asc'}
-                onClick={() => handleSort('grado')}
-              >
-                Grado
-              </TableSortLabel>
-            </TableCell>
-            <TableCell>
-              <TableSortLabel
-                active={sortConfig.field === 'comentarios'}
-                direction={sortConfig.field === 'comentarios' ? sortConfig.direction : 'asc'}
-                onClick={() => handleSort('comentarios')}
+                active={sortConfig.field === "comentarios"}
+                direction={
+                  sortConfig.field === "comentarios"
+                    ? sortConfig.direction
+                    : "asc"
+                }
+                onClick={() => handleSort("comentarios")}
               >
                 Comentarios
               </TableSortLabel>
@@ -271,21 +284,23 @@ const Buscador = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {sortedRespuestas && sortedRespuestas.map((respuesta) => (
+          {sortedRespuestas.map((respuesta) => (
             <TableRow key={respuesta.id}>
               <TableCell>{respuesta.id}</TableCell>
               <TableCell>{respuesta.createdAt}</TableCell>
               <TableCell>{respuesta.dependenciaId}</TableCell>
               <TableCell>{respuesta.edad}</TableCell>
               <TableCell>{respuesta.genero}</TableCell>
-              <TableCell>{respuesta.expresion}</TableCell>
-              <TableCell>{respuesta.calificaciones}</TableCell>
-              <TableCell>{respuesta.clasificaciones}</TableCell>
-              <TableCell>{respuesta.grado}</TableCell>
+              {respuesta.expresion }
+              {respuesta.calificaciones }
+              
+              {respuesta.clasificaciones}
+              {respuesta.grado}
               <TableCell>
-                {respuesta.comentarios && respuesta.comentarios.map((comentario) => (
-                  <div key={comentario.id}>{comentario.comentario}</div>
-                ))}
+                {respuesta.comentarios &&
+                  respuesta.comentarios.map((comentario) => (
+                    <div key={comentario.id}>{comentario.comentario}</div>
+                  ))}
               </TableCell>
             </TableRow>
           ))}
