3ffde78d8d3516d8935808ae87bc24d3840a25c2
Marcelo Agustin Compiani Flores <marcelo.compiani.flores@pjn.gov.ar>
2023-06-16 13:34:54 -0300

    cambios varios

--- src/components/estadisticas.jsx
+++ src/components/estadisticas.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   Typography,
   FormControl,
@@ -8,60 +8,63 @@ import {
   Grid,
   Box,
   Divider,
-} from '@material-ui/core';
+} from "@material-ui/core";
 import "../css/global.css";
-import { Bar, Pie, Doughnut, PolarArea } from 'react-chartjs-2';
+import { Bar, Pie, Doughnut, PolarArea } from "react-chartjs-2";
 import useStyles from "../styles/estadisticasStyle";
-
 
 const Estadisticas = () => {
   const classes = useStyles();
   const [dependencias, setDependencias] = useState([]);
   const [preguntas, setPreguntas] = useState([]);
-  const [selectedDependencia, setSelectedDependencia] = useState('');
-  const [selectedPregunta, setSelectedPregunta] = useState('');
+  const [selectedDependencia, setSelectedDependencia] = useState("");
+  const [selectedPregunta, setSelectedPregunta] = useState("");
 
-  const [expresionData, setExpresionData] = useState('');
-  const [edadData, setEdadData] = useState('');
-  const [generoData, setGeneroData] = useState('');
-  const [clasificacionData, setClasificacionData] = useState('');
-  const [calificacionData, setCalificacionData] = useState('');
-  const [gradoData, setGradoData] = useState('');
+  const [respuestas, setRespuestas] = useState([]); // Agregado
 
-   useEffect(() => {
+  const [expresionData, setExpresionData] = useState("");
+  const [edadData, setEdadData] = useState("");
+  const [generoData, setGeneroData] = useState("");
+  const [clasificacionData, setClasificacionData] = useState("");
+  const [calificacionData, setCalificacionData] = useState("");
+  const [gradoData, setGradoData] = useState("");
+
+  useEffect(() => {
     const obtenerPreguntas = async () => {
       try {
-        const response = await fetch('http://localhost:3000/preguntas');
+        const response = await fetch("http://localhost:3000/preguntas");
         const data = await response.json();
         setPreguntas(data);
-        console.log('Preguntas obtenidas:', data);
+        console.log("Preguntas obtenidas:", data);
       } catch (error) {
-        console.error('Error al obtener las preguntas:', error);
+        console.error("Error al obtener las preguntas:", error);
       }
+      fetchRespuestas();
     };
 
     const obtenerDependencias = async () => {
       try {
-        const response = await fetch('http://localhost:3000/dependencias');
+        const response = await fetch("http://localhost:3000/dependencias");
         const data = await response.json();
         setDependencias(data);
-        console.log('Dependencias obtenidas:', data);
+        console.log("Dependencias obtenidas:", data);
       } catch (error) {
-        console.error('Error al obtener las dependencias:', error);
+        console.error("Error al obtener las dependencias:", error);
       }
     };
 
-    obtenerPreguntas(); 
-    obtenerDependencias(); 
+    obtenerPreguntas();
+    obtenerDependencias();
   }, []);
 
-  useEffect(() => {
-    const fetchRespuestas = async () => {
-      try {
+  const fetchRespuestas = async () => {
+    try {
+      if (selectedDependencia !== "" && selectedPregunta !== "") {
         const response = await fetch(`http://localhost:3000/respuestas?dependenciaId=${selectedDependencia}&preguntaId=${selectedPregunta}`);
         const data = await response.json();
+        setRespuestas(data); // Almacena las respuestas obtenidas en el estado
 
-        const expresionCount = data.reduce((count, respuesta) => {
+        const expresionCount = respuestas.reduce((count, respuesta) => { // Actualizado a 'respuestas' en lugar de 'data'
           const respuestaExpresion = respuesta.expresion || 'NO_SE';
           count[respuestaExpresion] = (count[respuestaExpresion] || 0) + 1;
           return count;
@@ -71,7 +74,7 @@ const Estadisticas = () => {
           labels: Object.keys(expresionCount),
           datasets: [
             {
-              label: 'Respuestas',
+              label: 'expresion',
               data: Object.values(expresionCount),
               backgroundColor: [
                 'rgba(75, 192, 192, 0.6)',
@@ -83,7 +86,7 @@ const Estadisticas = () => {
           ],
         });
 
-        const edadCount = data.reduce((count, respuesta) => {
+        const edadCount = respuestas.reduce((count, respuesta) => { // Actualizado a 'respuestas' en lugar de 'data'
           const edad = respuesta.edad;
           count[edad] = (count[edad] || 0) + 1;
           return count;
@@ -100,7 +103,7 @@ const Estadisticas = () => {
           ],
         });
 
-        const generoCount = data.reduce((count, respuesta) => {
+        const generoCount = respuestas.reduce((count, respuesta) => { // Actualizado a 'respuestas' en lugar de 'data'
           const genero = respuesta.genero || 'OTRO';
           count[genero] = (count[genero] || 0) + 1;
           return count;
@@ -121,7 +124,7 @@ const Estadisticas = () => {
           ],
         });
 
-        const clasificacionCount = data.reduce((count, respuesta) => {
+        const clasificacionCount = respuestas.reduce((count, respuesta) => { // Actualizado a 'respuestas' en lugar de 'data'
           const clasificacion = respuesta.clasificaciones || 'NUNCA';
           count[clasificacion] = (count[clasificacion] || 0) + 1;
           return count;
@@ -142,19 +145,19 @@ const Estadisticas = () => {
               ],
             },
           ],
-        })
+        });
 
-        const gradoCount = data.reduce((count, respuesta) => {
-          const grado = respuesta.grado || 'ALTA';
-          count[grado] = (count[grado] || 0) + 1;
+        const gradoCount = respuestas.reduce((count, respuesta) => { // Actualizado a 'respuestas' en lugar de 'data'
+          const calificacion = respuesta.calificacion || 'MALO';
+          count[calificacion] = (count[calificacion] || 0) + 1;
           return count;
         }, {});
 
-        setGradoData({
+        setCalificacionData({
           labels: Object.keys(gradoCount),
           datasets: [
             {
-              label: 'grado',
+              label: 'Calificación',
               data: Object.values(gradoCount),
               backgroundColor: [
                 'rgba(75, 192, 192, 0.6)',
@@ -165,106 +168,79 @@ const Estadisticas = () => {
               ],
             },
           ],
-        })
-
-        const calificacionCount = data.reduce((count, respuesta) => {
-          const calificacion = respuesta.calificaciones || 'REGULAR';
-          count[calificacion] = (count[calificacion] || 0) + 1;
-          return count;
-        }, {});
-
-        setCalificacionData({
-          labels: Object.keys(calificacionCount),
-          datasets: [
-            {
-              label: 'Calificación',
-              data: Object.values(calificacionCount),
-              backgroundColor: [
-                'rgba(75, 192, 192, 0.6)',
-                'rgba(255, 99, 132, 0.6)',
-                'rgba(54, 162, 235, 0.6)',
-                'rgba(255, 206, 86, 0.6)',
-                'rgba(153, 102, 255, 0.6)',
-              ],
-            },
-          ],
         });
-
-
-      } catch (error) {
-        console.error('Error al obtener las respuestas:', error);
+      } else {
+        setExpresionData(null);
+        setEdadData(null);
+        setGeneroData(null);
+        setClasificacionData(null);
+        setCalificacionData(null);
       }
-    };
-
-    if (selectedDependencia && selectedPregunta) {
-      fetchRespuestas();
-    } else {
-      setExpresionData(null);
-      setEdadData(null);
-      setGeneroData(null);
-      setClasificacionData(null);
-      setCalificacionData(null);
+    } catch (error) {
+      console.error('Error fetching respuestas:', error);
     }
-  }, [selectedDependencia, selectedPregunta]);
-  
+  };
+
   const handlePreguntaChange = (event) => {
     setSelectedPregunta(event.target.value);
-    console.log('Pregunta seleccionada:', event.target.value);
+    fetchRespuestas(); // Fetch responses when pregunta changes
   };
 
   const handleDependenciaChange = (event) => {
     setSelectedDependencia(event.target.value);
-    console.log('Dependencia seleccionada:', event.target.value);
+    fetchRespuestas(); // Fetch responses when dependencia changes
   };
-
   return (
-    <Grid container  justifyContent="center" alignItems="center">
+    <Grid container justifyContent="center" alignItems="center">
       <Grid item xs={12}>
-          <Typography variant="h4" align="center" className={classes.titulo}>
-            Estadísticas
-          </Typography>
-          <Divider />
+        <Typography variant="h4" align="center" className={classes.titulo}>
+          Estadísticas
+        </Typography>
+        <Divider />
       </Grid>
       <Grid item xs={12} md={4}>
         <Box mx={4}>
-        <FormControl variant="standard" fullWidth size="small">
-              <InputLabel>Pregunta</InputLabel>
-              <Select
-                value={selectedPregunta}
-                onChange={handlePreguntaChange}
-                variant="standard"
-                className={classes.select}
-                fullWidth
-              >
-                <MenuItem value="">Todas las preguntas</MenuItem>
-                {preguntas.length > 0 && preguntas.map((pregunta) => (
+          <FormControl variant="standard" fullWidth size="small">
+            <InputLabel>Pregunta</InputLabel>
+            <Select
+              value={selectedPregunta}
+              onChange={handlePreguntaChange}
+              variant="standard"
+              className={classes.select}
+              fullWidth
+              disabled={!selectedDependencia} // Agregar esta línea
+            >
+              <MenuItem value="">Todas las preguntas</MenuItem>
+              {preguntas.length > 0 &&
+                preguntas.map((pregunta) => (
                   <MenuItem key={pregunta.id} value={pregunta.id}>
                     {pregunta.descripcion}
                   </MenuItem>
                 ))}
-              </Select>
-            </FormControl>
+            </Select>
+          </FormControl>
         </Box>
       </Grid>
       <Grid item xs={12} md={4}>
         <Box mx={4}>
-        <FormControl variant="standard" fullWidth size="small">
-              <InputLabel>Dependencia</InputLabel>
-              <Select
-                value={selectedDependencia}
-                onChange={handleDependenciaChange}
-                variant="standard"
-                className={classes.select}
-                fullWidth
-              >
-                <MenuItem value="">Todas las dependencias</MenuItem>
-                {dependencias.length > 0 && dependencias.map((dependencia) => (
+          <FormControl variant="standard" fullWidth size="small">
+            <InputLabel>Dependencia</InputLabel>
+            <Select
+              value={selectedDependencia}
+              onChange={handleDependenciaChange}
+              variant="standard"
+              className={classes.select}
+              fullWidth
+            >
+              <MenuItem value="">Todas las dependencias</MenuItem>
+              {dependencias.length > 0 &&
+                dependencias.map((dependencia) => (
                   <MenuItem key={dependencia.id} value={dependencia.id}>
                     {dependencia.nombreDependencia}
                   </MenuItem>
                 ))}
-              </Select>
-            </FormControl>
+            </Select>
+          </FormControl>
         </Box>
       </Grid>
       <Grid item xs={12} md={5}>
@@ -344,7 +320,6 @@ const Estadisticas = () => {
           )}
         </Box>
       </Grid>
-      
     </Grid>
   );
 };
