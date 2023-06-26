d4aa813b5154b797efc7bc7140b4c64b96ee9cde
Marcelo Agustin Compiani Flores <marcelo.compiani.flores@pjn.gov.ar>
2023-06-26 12:51:18 -0300

    dsadsadsa

--- src/auth.js
+++ src/auth.js
@@ -4,14 +4,7 @@ export const setLoginData = (username, clave, rol, dependenciaId) => {
   sessionStorage.setItem(AUTH_TOKEN_KEY, 'true');
   sessionStorage.setItem("dependencia", username);
   sessionStorage.setItem("rol", rol);
-
-  if (dependenciaId) {
-    sessionStorage.setItem("dependenciaId", dependenciaId.toString()); // Convertir a cadena de texto si es necesario
-  } else {
-    console.error('Valor de dependenciaId no válido:', dependenciaId);
-  }
 }
-
 
 export const logout = () => {
   sessionStorage.removeItem(AUTH_TOKEN_KEY);
