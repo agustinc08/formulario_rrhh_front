d0d55e2eb4b1378eee8ed5e9ea7a02c525970eef
Marcelo Agustin Compiani Flores <marcelo.compiani.flores@pjn.gov.ar>
2023-07-10 10:01:56 -0300

    Cambios varios funcionales.

--- src/components/formulario.jsx
+++ src/components/formulario.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,  useRef, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import {
 	Typography,
@@ -20,7 +20,6 @@ import Pagination from "@material-ui/lab/Pagination";
 import "../css/global.css";
 import "../css/formulario.css";
 import useStyles from "../styles/formularioStyle";
-import { useHistory } from "react-router-dom";
 
 function Preguntas() {
 	const classes = useStyles();
@@ -38,12 +37,10 @@ function Preguntas() {
 	const [currentPage, setCurrentPage] = useState(1);
 	const [respuestas, setRespuestas] = useState({});
 	const [tipoRespuesta, setTipoRespuesta] = useState({});
-	const [mensaje, setMensaje] = useState(null);
 	const [openSnackbar, setOpenSnackbar] = useState(false);
 	const [snackbarMessage, setSnackbarMessage] = useState("");
 	const [snackbarSeverity, setSnackbarSeverity] = useState("");
 	const [error, setError] = useState(false);
-	const [preguntasSinSeleccion, setPreguntasSinSeleccion] = useState(false);
 	const [preguntasSinResponder, setPreguntasSinResponder] = useState({});
 	const [preguntaActual, setPreguntaActual] = useState(null);
 
@@ -58,55 +55,6 @@ function Preguntas() {
 		}
 		fetchData();
 	}, []);
-
-	useEffect(() => {
-		async function cargarSecciones() {
-			try {
-				const { data } = await axios.get("http://localhost:4000/secciones");
-				setSecciones(data);
-				setSeccionId(data[0]?.id); // Establecer la primera sección como sección actual
-			} catch (error) {
-				console.error(error);
-			}
-		}
-		cargarSecciones();
-	}, []);
-
-	const cargarPreguntasPorSeccion = useCallback(async () => {
-		if (seccionId) {
-		  try {
-			const response = await axios.get(
-			  `http://localhost:4000/preguntas/${seccionId}`
-			);
-			setPreguntasPorSeccion((prevPreguntasPorSeccion) => ({
-			  ...prevPreguntasPorSeccion,
-			  [seccionId]: response.data,
-			}));
-			setCurrentPage(1);
-			setPreguntaActual(response.data[0]?.id); // Establecer la primera pregunta como pregunta actual
-		  } catch (error) {
-			console.error(error);
-		  }
-		}
-	  }, [seccionId]);
-	  
-	  useEffect(() => {
-		cargarPreguntasPorSeccion();
-	  }, [cargarPreguntasPorSeccion]);
-	  
-	  useEffect(() => {
-		if (preguntasPorSeccion[seccionId]) {
-		  const startIndex = (currentPage - 1) * 5;
-		  const endIndex = startIndex + 5;
-		  const slicedPreguntas = preguntasPorSeccion[seccionId]?.slice(
-			startIndex,
-			endIndex
-		  );
-		  setPreguntas(slicedPreguntas);
-		} else {
-		  cargarPreguntasPorSeccion();
-		}
-	  }, [seccionId, currentPage, preguntasPorSeccion, cargarPreguntasPorSeccion]);
 
 	useEffect(() => {
 		async function fetchTipoRespuesta() {
@@ -130,6 +78,50 @@ function Preguntas() {
 		}
 		fetchTipoRespuesta();
 	}, []);
+
+	useEffect(() => {
+		async function cargarSecciones() {
+			try {
+				const { data } = await axios.get("http://localhost:4000/secciones");
+				setSecciones(data);
+				setSeccionId(data[0]?.id); // Establecer la primera sección como sección actual
+			} catch (error) {
+				console.error(error);
+			}
+		}
+		cargarSecciones();
+	}, []);
+
+	const cargarPreguntasPorSeccion = useCallback(async () => {
+		try {
+			const response = await axios.get(`http://localhost:4000/preguntas/${seccionId}`);
+			setPreguntasPorSeccion((prevPreguntasPorSeccion) => ({
+				...prevPreguntasPorSeccion,
+				[seccionId]: response.data,
+			}));
+			setCurrentPage(1);
+			setPreguntaActual(response.data[0]?.id); // Establecer la primera pregunta como pregunta actual
+		} catch (error) {
+			console.error(error);
+		}
+	}, [seccionId]);
+	
+	useEffect(() => {
+		(async () => {
+			await cargarPreguntasPorSeccion();
+		})();
+	}, [cargarPreguntasPorSeccion]);
+	  
+	useEffect(() => {
+		if (preguntasPorSeccion[seccionId]) {
+			const startIndex = (currentPage - 1) * 5;
+			const endIndex = startIndex + 5;
+			const slicedPreguntas = preguntasPorSeccion[seccionId]?.slice(startIndex, endIndex);
+			setPreguntas(slicedPreguntas);
+		} else {
+			cargarPreguntasPorSeccion();
+		}
+	}, [seccionId, currentPage, preguntasPorSeccion, cargarPreguntasPorSeccion]);
 
 	function handleEdadChange(event) {
 		setEdad(event.target.value);
@@ -163,137 +155,56 @@ function Preguntas() {
 		}
 	}
 
-	function validarFormulario() {
-		// Verificar si todas las preguntas tienen un tipo de respuesta
-		const preguntasSinTipoRespuesta = preguntas.filter(
-			(pregunta) => !respuestas[pregunta.id]?.tipoRespuesta
-		);
-
-		// Verificar si hay comentarios incompletos
-		const preguntasConComentarioIncompleto = preguntas.filter(
-			(pregunta) =>
-				pregunta.tieneComentario &&
-				!userComments[pregunta.id]?.trim() &&
-				respuestas[pregunta.id]?.tipoRespuesta
-		);
-
-		if (preguntasSinTipoRespuesta.length > 0 || preguntasConComentarioIncompleto.length > 0) {
-			setError(true);
-			setPreguntasSinSeleccion(preguntasSinTipoRespuesta.length > 0);
-			setPreguntasSinResponder(
-				preguntasConComentarioIncompleto.reduce((acc, pregunta) => {
-					acc[pregunta.id] = true;
-					return acc;
-				}, {})
-			);
-			return false;
-		}
-
-		setError(false);
-		setPreguntasSinSeleccion(false);
-		setPreguntasSinResponder({});
-		return true;
-	}
 
 	async function enviarRespuestas(event) {
 		event.preventDefault();
-
-		if (!preguntaActual || !preguntas) {
-			return;
+		try {
+		  const formularioId = await getFormularioActivo();
+		  if (!formularioId) {
+			throw new Error("No hay formulario activo");
+		  }
+		  const preguntasRespuestas = Object.entries(respuestas).map(
+			([preguntaId, respuesta]) => ({
+			  preguntaId: parseInt(preguntaId),
+			  formularioId,
+			  tipoRespuestaId: respuesta.tipoRespuesta,
+			  dependenciaId: dependencia,
+			  edad,
+			  genero,
+			  comentario: {
+				respuestaComentario: userComments[preguntaId],
+				preguntaId: parseInt(preguntaId),
+				respuestaId: respuesta.tipoRespuesta,
+				formularioId,
+				dependenciaId: dependencia,
+			  },
+			})
+		  );
+		  await axios.post("http://localhost:4000/respuestas", {
+			preguntasRespuestas,
+		  });
+		  setSnackbarMessage("Respuestas enviadas correctamente");
+		  setSnackbarSeverity("success");
+		} catch (error) {
+		  console.error(error);
+		  setSnackbarMessage("Error al enviar las respuestas");
+		  setSnackbarSeverity("error");
+		} finally {
+		  setOpenSnackbar(true);
 		}
-
-		const esFormularioValido = validarFormulario();
-
-		if (esFormularioValido) {
-			try {
-				const formularioId = await getFormularioActivo();
-
-				const createRespuestaDto = {
-					preguntasRespuestas: preguntas.map((pregunta) => {
-						return {
-							preguntaId: pregunta.id,
-							formularioId: formularioId,
-							tipoRespuestaId: respuestas[pregunta.id]?.tipoRespuesta || null,
-							dependenciaId: dependencia,
-							edad: edad,
-							genero: genero,
-							comentario: {
-								respuestaComentario: userComments[pregunta.id] || "",
-								preguntaId: pregunta.id,
-								respuestaId: null, // El ID de la respuesta se asignará después de crearla
-								formularioId: formularioId,
-								dependenciaId: dependencia,
-							},
-						};
-					}),
-				};
+	  }
 
 
-				console.log(createRespuestaDto); // Verificar los valores aquí
-
-				const response = await axios.post(
-					"http://localhost:4000/respuestas",
-					createRespuestaDto
-				);
-
-				const respuestasCreadas = response.data;
-
-				// Actualizar los IDs de las respuestas en el objeto createRespuestaDto
-				const createRespuestaDtoConRespuestaIds = createRespuestaDto.preguntasRespuestas.map(
-					(preguntaRespuesta, index) => {
-						preguntaRespuesta.comentario.respuestaId = respuestasCreadas[index].id;
-						return preguntaRespuesta;
-					}
-				);
-
-				// Enviar el objeto actualizado con los IDs de las respuestas
-				await axios.post(
-					"http://localhost:4000/respuestas",
-					{ preguntasRespuestas: createRespuestaDtoConRespuestaIds }
-				);
-
-				setEdad("");
-				setGenero("");
-				setDependencia("");
-				setUserComments({});
-				setRespuestas({});
-				setSnackbarMessage("Respuestas enviadas correctamente");
-				setSnackbarSeverity("success");
-				setOpenSnackbar(true);
-			} catch (error) {
-				console.error(error);
-				setSnackbarMessage("Error al enviar las respuestas");
-				setSnackbarSeverity("error");
-				setOpenSnackbar(true);
-			}
-		}
-	}
 
 	return (
 		<Container className="mb80px">
 			<Typography variant="h4" className={classes.tituloPregunta}>
-				{mensaje}
 				{seccionId &&
 					secciones.length > 0 &&
 					secciones.find((seccion) => seccion.id === seccionId)?.descripcion}
 			</Typography>
 			<Divider />
 			<form onSubmit={enviarRespuestas} className="formulario">
-				{error && (
-					<Typography variant="body1" color="error">
-						Por favor, selecciona una opción en todas las preguntas.
-					</Typography>
-				)}
-				{preguntasSinSeleccion && (
-					<Typography variant="body1" color="error">
-						Por favor, selecciona una opción en todas las preguntas.
-					</Typography>
-				)}
-				{Object.keys(preguntasSinResponder).length > 0 && (
-					<Typography variant="body1" color="error">
-						Por favor, completa todos los comentarios.
-					</Typography>
-				)}
 				{isFirstPage && (
 					<>
 						<Grid container spacing={3}>
@@ -426,6 +337,7 @@ function Preguntas() {
 																	value={userComments[pregunta.id] || ""}
 																	onChange={(event) =>
 																		handleComentarioChange(event, pregunta.id)
+																		
 																	}
 																	fullWidth
 																	multiline
